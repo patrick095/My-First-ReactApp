d025f2d626c5e4ae814a81805d4374578ca8ab61
Patrick <patrick095@gmail.com>
2021-05-03 13:37:03 -0300

    update project

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,62 +1,55 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import api from '../../services/api';
 import { Link } from 'react-router-dom';
 
 import "./styles.css";
 
-export default class Main extends Component {
-    state = {
+function Main() {
+    const defaultState = {
         products: [],
         productInfo: {},
         page: 1,
     }
+    const [state, setState] = useState(defaultState)
+    useEffect(()=>{loadProducts()},[])
 
-    componentDidMount() {
-        this.loadProducts();
-    }
+    const { products, page, productInfo } = state
 
-    loadProducts = async (page = 1) => {
+    async function loadProducts (page = 1){
         const response = await api.get(`/products?page=${page}`)
         const { docs, ...productInfo } = response.data
-        this.setState({ products: docs, productInfo, page})
+        setState({ products: docs, productInfo, page})
     }
-    prevPage = () => {
-        const { page } = this.state
-
+    function prevPage() {
         if (page === 1) return;
 
         const pageNumber = page - 1;
 
-        this.loadProducts(pageNumber)
+        loadProducts(pageNumber)
     }
-    nextPage = () => {
-        const { page, productInfo } = this.state
-
+    function nextPage (){
         if (page === productInfo.pages) return;
         
         const pageNumber = page + 1;
 
-        this.loadProducts(pageNumber)
+        loadProducts(pageNumber)
             
     }
+    return (
+        <div className='product-list'>
+            {products.map(product => (
+                <article key={product._id}>
+                    <strong>{product.title}</strong>
+                    <p>{product.description}</p>
 
-    render() {
-        const { products, page, productInfo } = this.state
-        return (
-            <div className='product-list'>
-                {products.map(product => (
-                    <article key={product._id}>
-                        <strong>{product.title}</strong>
-                        <p>{product.description}</p>
-
-                        <Link to={`/products/${product._id}`}>Acessar</Link>
-                    </article>
-                ))}
-                <div className="actions">
-                    <button disabled={page === 1} onClick={this.prevPage}>Anterior</button>
-                    <button disabled={page === productInfo.pages} onClick={this.nextPage}>Próxima</button>
-                </div>
+                    <Link to={`/products/${product._id}`}>Acessar</Link>
+                </article>
+            ))}
+            <div className="actions">
+                <button disabled={page === 1} onClick={prevPage}>Anterior</button>
+                <button disabled={page === productInfo.pages} onClick={nextPage}>Próxima</button>
             </div>
-        )
+        </div>
+    )
     }
-}
+export default Main;
--- src/pages/product/index.js
+++ src/pages/product/index.js
@@ -1,34 +1,38 @@
-import React, { Component } from 'react';
-import api from '../../services/api'
+import React, { useEffect, useState } from 'react';
+import api from '../../services/api';
 import { Link } from 'react-router-dom';
 
 import "./styles.css";
 
-export default class Product extends Component {
-    state = {
+function Product (props){
+    const defaultState = {
         product: {},
+    };
+    const [state, setState] = useState(defaultState);
+    const { product} = state;
+    
+    useEffect(()=>{
+        async function fetchData(){
+            const { id } = props.match.params;
+            const response = await api.get(`/products/${id}`);
+    
+            setState({ product: response.data});
+            return response
+        };
+        fetchData();
+    },[props.match.params])
+
+    return (
+        <div className="product-info">
+            <h1>{product.title}</h1>
+            <p>{product.description}</p>
+
+            <p>
+                URL: <a href={product.url}>{product.url}</a>
+            </p>
+            <Link to='/' className='back'>Voltar</Link>
+
+        </div>
+    )
     }
-
-    async componentDidMount(){
-        const { id } = this.props.match.params
-        const response = await api.get(`/products/${id}`);
-
-        this.setState({ product: response.data})
-    }
-    render() {
-        const { product} = this.state
-
-        return (
-            <div className="product-info">
-                <h1>{product.title}</h1>
-                <p>{product.description}</p>
-
-                <p>
-                    URL: <a href={product.url}>{product.url}</a>
-                </p>
-                <Link to='/' className='back'>Voltar</Link>
-
-            </div>
-        )
-    }
-}
+export default Product;
